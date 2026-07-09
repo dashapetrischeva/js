bb07d866b9c5ce34ad01027ca77dc242e17b7180
dashapetrischeva <151852045+dashapetrischeva@users.noreply.github.com>
2026-07-09 14:16:31 +0200

    Add files via upload
    Translation for homework 18

--- lesson_18/js/script3.js
+++ lesson_18/js/script3.js
@@ -8,6 +8,12 @@ window.onload = function () {
 function getAge() {
 	const currentYear = 2025
 	const birthYear = parseInt(document.getElementById('birthYear').value)
-	const userAge = currentYear - birthYear
-	document.getElementById('userAge').value = userAge
+	if (isNaN(birthYear) || birthYear < 0 || birthYear > currentYear) {
+		document.getElementById('birthYearError').innerText = 'Incorrect year'
+		document.getElementById('userAge').value = ''
+	} else {
+		const userAge = currentYear - birthYear
+		document.getElementById('userAge').value = userAge
+		document.getElementById('birthYearError').innerText = ''
+	}
 }
